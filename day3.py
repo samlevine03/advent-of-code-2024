0e5765879802d5a9aa03c13117f3a2441b638ba4
Sam Levine <samlevine03@gmail.com>
2024-12-03 16:56:48 -0600

    luckbox

--- day3.py
+++ day3.py
@@ -18,6 +18,9 @@ with open("day3.txt") as input_file:
     print("res1", res1)
 
     input = re.sub(r"don't\(\).*?do\(\)", "", input)
+    # NOTE: there is an edge case if the last instruction is a 
+    # don't() where we also need to remove from don't()-EOF. 
+    # but i didn't have it so i never wrote the regex lol
 
     matches = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", input)
     res2 = sum(map(mult_match, matches))
