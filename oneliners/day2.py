de1316dd47d84f6da2ae9217aed64db4bf4cec39
Sam Levine <samlevine03@gmail.com>
2024-12-04 16:16:23 -0600

    cleaned day 2

--- oneliners/day2.py
+++ oneliners/day2.py
@@ -1,9 +1,9 @@
 reports = [[int(x) for x in line.split()] for line in open('../inputs/day2.txt')]
 
 def is_valid(r):
-    inc = r[0] < r[1]
-    return all(0 < abs(r[i] - r[i-1]) <= 3 and (r[i] > r[i-1]) == inc for i in range(1, len(r)))
+    return all(0 < abs(r[i] - r[i-1]) <= 3 and (r[i] > r[i-1]) == (r[0] < r[1]) for i in range(1, len(r)))
 
 ans1 = sum(is_valid(r) for r in reports)
 ans2 = sum(is_valid(r) or any(is_valid(r[:i] + r[i+1:]) for i in range(len(r))) for r in reports)
+
 print(f"{ans1}\n{ans2}")
