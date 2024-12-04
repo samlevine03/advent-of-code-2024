cbb406eecac6c62efc2d591ddc91c0f721fbefdf
Sam Levine <samlevine03@gmail.com>
2024-12-04 16:19:56 -0600

    format

--- oneliners/day3.py
+++ oneliners/day3.py
@@ -4,6 +4,8 @@ input = input = re.sub(r"\n", "", open("../inputs/day3.txt").read())
 enabled_input = re.sub(r"don't\(\).*?(do\(\)|$)", "", input)
 
 ans1 = sum(int(x) * int(y) for x, y in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", input))
-ans2 = sum(int(x) * int(y) for x, y in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", enabled_input))
+ans2 = sum(
+    int(x) * int(y) for x, y in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", enabled_input)
+)
 
 print(f"{ans1}\n{ans2}")
--- oneliners/day4.py
+++ oneliners/day4.py
@@ -1,18 +1,36 @@
-matrix = [list(row) for row in open('../inputs/day4.txt').read().split('\n')]
+matrix = [list(row) for row in open("../inputs/day4.txt").read().split("\n")]
 
 def count_xmas(r, c):
-    xmas = 'XMAS'
     return sum(
-        all(0 <= r + dr * i < len(matrix) and 0 <= c + dc * i < len(matrix[0]) and matrix[r + dr * i][c + dc * i] == xmas[i]
-            for i in range(1, len(xmas))) for dr in [-1,0,1] for dc in [-1,0,1]
+        all(
+            0 <= r + dr * i < len(matrix)
+            and 0 <= c + dc * i < len(matrix[0])
+            and matrix[r + dr * i][c + dc * i] == "XMAS"[i]
+            for i in range(1, 4)
+        )
+        for dr in [-1, 0, 1]
+        for dc in [-1, 0, 1]
     )
 
-ans1 = sum(count_xmas(r, c) for r in range(len(matrix)) for c in range(len(matrix[0])) if matrix[r][c] == 'X')
+ans1 = sum(
+    count_xmas(r, c)
+    for r in range(len(matrix))
+    for c in range(len(matrix[0]))
+    if matrix[r][c] == "X"
+)
 
 def count_x_mas(r, c):
-    return int(matrix[r][c] == "A" and 
-               {"M", "S"} == {matrix[r-1][c-1], matrix[r+1][c+1]} == {matrix[r-1][c+1], matrix[r+1][c-1]})
+    return int(
+        matrix[r][c] == "A"
+        and {"M", "S"}
+        == {matrix[r - 1][c - 1], matrix[r + 1][c + 1]}
+        == {matrix[r - 1][c + 1], matrix[r + 1][c - 1]}
+    )
 
-ans2 = sum(count_x_mas(r, c) for r in range(1, len(matrix) - 1) for c in range(1, len(matrix[0]) - 1))
+ans2 = sum(
+    count_x_mas(r, c)
+    for r in range(1, len(matrix) - 1)
+    for c in range(1, len(matrix[0]) - 1)
+)
 
 print(f"{ans1}\n{ans2}")
