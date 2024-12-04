e7f7ac45dafc28e3b734b927e3b837c7041f97db
Sam Levine <samlevine03@gmail.com>
2024-12-04 16:06:23 -0600

    open change

--- oneliners/day3.py
+++ oneliners/day3.py
@@ -1,8 +1,7 @@
 import re
 
-with open("../inputs/day3.txt") as f:
-    input = re.sub(r"\n", "", f.read())
-    enabled_input = re.sub(r"don't\(\).*?(do\(\)|$)", "", input)
+input = input = re.sub(r"\n", "", open("../inputs/day3.txt").read())
+enabled_input = re.sub(r"don't\(\).*?(do\(\)|$)", "", input)
 
 ans1 = sum(int(x) * int(y) for x, y in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", input))
 ans2 = sum(int(x) * int(y) for x, y in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", enabled_input))
